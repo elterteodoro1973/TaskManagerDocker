9f8ed594855c054954b391795400e90dfe0268fa
elters <elters@DESKTOP-TM9506H>
2025-02-14 12:47:28 -0300

    Inicio

---
+++ TaskManager.API/Data/Configurations/DatabaseConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.API.Data.Configurations
+{
+    public class DatabaseConfig : IDatabaseConfig
+    {
+        public string DatabaseName { get;  set; }
+        public string ConnectionString { get; set; }
+
+        
+    }
+}
